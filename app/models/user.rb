943de05e126853474c9fe07b2c587cb1fdecfcf6
Joao Brufatto <joao.brufatto@gmail.com>
2021-03-02 12:08:16 -0300

    Added omniauth with Twitch

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,24 @@ class User < ApplicationRecord
   has_many :favorites
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: %i[twitch]
 
   validates :email, presence: true
   validates :encrypted_password, presence: true
   validates :nickname, presence: true
   validates :image, presence: true
   validates :view_count, presence: true
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.name = auth.info.name # assuming the user model has a name
+      user.nickname = auth.info.nickname # assuming the user model has an image
+      # If you are using confirmable and the provider(s) you use validate emails,
+      # uncomment the line below to skip the confirmation emails.
+      user.skip_confirmation!
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Rotas do Devise
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   # Rotas de Bots e Configs
   resources :bots, only: [:create, :edit, :index, :new, :show, :update] do
     resources :configs, only: [:create, :edit, :new, :update]
@@ -10,6 +10,9 @@ Rails.application.routes.draw do
 
   # Rotas de Favorites
   resources :favorites, only: [:index, :create, :destroy]
+
+  # Rota de Login Twitch
+  # get '/auth/twitch/callback' => 'sessions#create'
 
   # Pagina Inicial
   root to: 'pages#home'
