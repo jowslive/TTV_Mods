7047eda5b816032a0fa7ed894d5d07d51920505b
kayo pereira <kayo.richard2000@gmail.com>
2021-03-04 14:17:28 -0300

    Started style in homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @bots = policy_scope(Bot).all.order(:created_at)
   end
 end
