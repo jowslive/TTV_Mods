c992cc8bc8b48cac445155fe05f88312cd35b2c9
Joao Brufatto <joao.brufatto@gmail.com>
2021-03-05 12:32:21 -0300

    Changed the language of Devise

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,8 @@ module TTVMods
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # Devise pt-br
+    config.i18n.default_locale = :'pt-BR'
   end
 end
