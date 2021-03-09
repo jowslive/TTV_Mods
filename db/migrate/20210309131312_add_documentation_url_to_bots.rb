b39bdd7aab148c41575eab299393932b95194b67
Joao Brufatto <joao.brufatto@gmail.com>
2021-03-09 12:21:47 -0300

    Updated Seeds File

--- db/migrate/20210309131312_add_documentation_url_to_bots.rb
+++ db/migrate/20210309131312_add_documentation_url_to_bots.rb
@@ -1,4 +1,4 @@
-class AddDocumentationUrlToBot < ActiveRecord::Migration[6.0]
+class AddDocumentationUrlToBots < ActiveRecord::Migration[6.0]
   def change
     add_column :bots, :documentation_url, :text
   end
